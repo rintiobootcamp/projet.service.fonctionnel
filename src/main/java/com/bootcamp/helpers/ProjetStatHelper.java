12a7ec45ca1a7e9e28a09f12c66fb928e1492320
fbr <bignonfebron@gmail.com>
2017-12-20 12:41:31 +0100

    logic implements:finished. first test:good. m2 problems

---
+++ src/main/java/com/bootcamp/helpers/ProjetStatHelper.java
@@ -0,0 +1,43 @@
+package com.bootcamp.helpers;
+
+import com.bootcamp.entities.Phase;
+
+import java.util.List;
+
+public class ProjetStatHelper {
+    private double tauxBuget;
+    private double tauxFinancementPrive;
+    private double tauxFinancementPublic;
+    private List<PhaseStatHelper> phaseStatHelperList;
+    public double getTauxBuget() {
+        return tauxBuget;
+    }
+
+    public void setTauxBuget(double tauxBuget) {
+        this.tauxBuget = tauxBuget;
+    }
+
+    public double getTauxFinancementPrive() {
+        return tauxFinancementPrive;
+    }
+
+    public void setTauxFinancementPrive(double tauxFinancementPrive) {
+        this.tauxFinancementPrive = tauxFinancementPrive;
+    }
+
+    public double getTauxFinancementPublic() {
+        return tauxFinancementPublic;
+    }
+
+    public void setTauxFinancementPublic(double tauxFinancementPublic) {
+        this.tauxFinancementPublic = tauxFinancementPublic;
+    }
+
+    public List<PhaseStatHelper> getPhaseStatHelperList() {
+        return phaseStatHelperList;
+    }
+
+    public void setPhaseStatHelperList(List<PhaseStatHelper> phaseStatHelperList) {
+        this.phaseStatHelperList = phaseStatHelperList;
+    }
+}
